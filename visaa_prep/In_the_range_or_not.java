07c7c016dea40adf07c0f7c7e6f62997f8246671
Srilekha2210 <gsrilekhagoud816@gmail.com>
2024-11-15 02:06:19 +0530

    Create In_the_range_or_not.java

---
+++ visaa_prep/In_the_range_or_not.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++){
+           if(a[i]>=67&&45000>=a[i]){
+               System.out.print("YES"+"\n");
+              
+           }
+            else{
+                System.out.print("NO"+"\n");
+            }
+        }
+        
+    }
+}
