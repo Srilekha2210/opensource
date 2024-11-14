f0f567231569434969c2d73c57f8e028fd457a7c
Srilekha2210 <gsrilekhagoud816@gmail.com>
2024-11-15 01:09:02 +0530

    Create Mirror_Magic.java

---
+++ visaa_prep/Mirror_Magic.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a[][]=new int[n][n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                a[i][j]=sc.nextInt();
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n/2;j++){
+            int t=a[i][j];
+            a[i][j]=a[i][n-1-j];
+            a[i][n-1-j]=t;}
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                System.out.print(a[i][j]+" ");
+                
+            }
+            System.out.print("\n");
+        }
+    }
+}
