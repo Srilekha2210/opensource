939363a643d53e0fdbb1458534ad88654eb8b009
Srilekha2210 <gsrilekhagoud816@gmail.com>
2024-11-16 12:08:13 +0530

    Create Transpose_Matrix.java

---
+++ visaa_prep/Transpose_Matrix.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a[][]=new int[n][n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                a[i][j]=sc.nextInt();
+            }
+        }
+        int b[][]=new int[n][n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                b[i][j]=a[j][i];
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                System.out.print(b[i][j]+" ");
+            }
+            System.out.print("\n");
+        }
+    }
+}
